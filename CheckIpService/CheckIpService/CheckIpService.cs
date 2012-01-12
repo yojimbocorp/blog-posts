25868cdd87956d5bade8272a5d9a7a916282c1b2
Ken Nickel <knickel@cox.net>
2012-01-11 16:20:58 -0800

    CheckIpService project

---
+++ CheckIpService/CheckIpService/CheckIpService.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Configuration;
+using System.Net;
+using System.Net.Mail;
+using System.ServiceProcess;
+using System.Timers;
+
+namespace CheckIpService
+{
+    public partial class CheckIpService : ServiceBase
+    {
+        private readonly Timer _timer = new Timer();
+
+        public CheckIpService()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            _timer.AutoReset = true;
+            _timer.Interval = Convert.ToDouble(ConfigurationManager.AppSettings["ServiceInterval"]);
+            _timer.Elapsed += OnElapsedTime;
+            _timer.Start();
+        }
+
+        private string _lastIp;
+        private void OnElapsedTime(object source, ElapsedEventArgs e)
+        {
+            try
+            {
+                var client = new WebClient();
+                var response = client.DownloadString("http://checkip.dyndns.org");
+                const string startToken = "Current IP Address: ";
+                var startIndex = response.IndexOf(startToken);
+                startIndex += startToken.Length;
+                var endIndex = response.IndexOf("</body>");
+                var ip = response.Substring(startIndex, endIndex - startIndex);
+
+                if (String.IsNullOrEmpty(_lastIp) || _lastIp != ip)
+                {
+                    _lastIp = ip;
+                    SendMail(ip);
+                }
+            }
+            catch (Exception ex)
+            {
+                EventLog.WriteEntry(ex.Message);
+            }
+        }
+
+        private void SendMail(string ip)
+        {
+            var appSettings = ConfigurationManager.AppSettings;
+
+            var email = Convert.ToString(appSettings["EmailAddress"]);
+            var displayName = Convert.ToString(appSettings["DisplayName"]);
+
+            var fromAddress = new MailAddress(email, displayName);
+            var toAddress = new MailAddress(email, displayName);
+            var fromPassword = Convert.ToString(appSettings["EmailPassword"]);
+            var subject = "Current Home IP Address: " + ip;
+            var body = subject;
+            var smtp = new SmtpClient
+                            {
+                                Host = Convert.ToString(appSettings["EmailHost"]),
+                                Port = Convert.ToInt32(appSettings["EmailPort"]),
+                                EnableSsl = Convert.ToBoolean(appSettings["EnableSsl"]),
+                                DeliveryMethod = SmtpDeliveryMethod.Network,
+                                UseDefaultCredentials = false,
+                                Credentials = new NetworkCredential(fromAddress.Address, fromPassword)
+                            };
+            using (var message = new MailMessage(fromAddress, toAddress)
+                                        {
+                                            Subject = subject,
+                                            Body = body
+                                        })
+            {
+                smtp.Send(message);
+            }
+        }
+
+        protected override void OnStop()
+        {
+            _timer.Stop();
+        }
+    }
+}
---
+++ CheckIpService/CheckIpService/ProjectInstaller.Designer.cs
@@ -0,0 +1,58 @@
+﻿namespace CheckIpService
+{
+    partial class ProjectInstaller
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.serviceProcessInstaller1 = new System.ServiceProcess.ServiceProcessInstaller();
+            this.CheckIpServiceInstaller = new System.ServiceProcess.ServiceInstaller();
+            // 
+            // serviceProcessInstaller1
+            // 
+            this.serviceProcessInstaller1.Account = System.ServiceProcess.ServiceAccount.LocalService;
+            this.serviceProcessInstaller1.Password = null;
+            this.serviceProcessInstaller1.Username = null;
+            // 
+            // CheckIpServiceInstaller
+            // 
+            this.CheckIpServiceInstaller.ServiceName = "CheckIpService";
+            this.CheckIpServiceInstaller.StartType = System.ServiceProcess.ServiceStartMode.Automatic;
+            // 
+            // ProjectInstaller
+            // 
+            this.Installers.AddRange(new System.Configuration.Install.Installer[] {
+            this.CheckIpServiceInstaller,
+            this.serviceProcessInstaller1});
+
+        }
+
+        #endregion
+
+        private System.ServiceProcess.ServiceProcessInstaller serviceProcessInstaller1;
+        private System.ServiceProcess.ServiceInstaller CheckIpServiceInstaller;
+    }
+}
