ea20aa1c6b0188ec62af5f51eeb4bb1f99b0625e
Ken Nickel <ken.nickel@gmail.com>
2012-05-28 21:22:19 -0700

    responsive ui using Task

---
+++ BusyIndicator/BusyIndicator/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Windows;
+using System.Threading.Tasks;
+using System.Windows.Threading;
+
+namespace BusyIndicator
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private System.Windows.Threading.DispatcherTimer _timer = null;
+        public MainWindow()
+        {
+            InitializeComponent();
+            _timer = new DispatcherTimer();
+            _timer.IsEnabled = false;
+
+            _timer.Tick += new System.EventHandler(RunTask);
+        }
+
+        private void StartProcess(object sender, RoutedEventArgs e)
+        {
+            _busyIndicator.IsBusy = true;
+            _timer.Interval = new TimeSpan(0,0,0,0,100);
+            _timer.Start();
+            // go back to the user thread, giving the busy indicator a chance to come up before the next timer tick event
+        }
+
+        void RunTask(object sender, System.EventArgs e)
+        {
+            _timer.IsEnabled = false;
+
+            var T = new Task(() =>
+            {
+                for (var i = 0; i < 100; i++)
+                {
+                    System.Threading.Thread.Sleep(50);
+                }
+            });
+
+            T.ContinueWith(antecedent => _busyIndicator.IsBusy = false,
+                                     TaskScheduler.FromCurrentSynchronizationContext()
+                );
+
+            T.Start();
+        }
+    }
+}
